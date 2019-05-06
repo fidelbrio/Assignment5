fc9d738c6d1d578785e2d078a6a0b45c1c356f39
greg0reo <31292320+greg0reo@users.noreply.github.com>
2019-05-05 20:53:56 -0400

    Update segment.cpp

--- segment.cpp
+++ segment.cpp
@@ -8,7 +8,27 @@ void initializeSegment(segment *s){
 	for(int i = 0; i<1024*1024;i++){
 		s->buffer[i] = '0';
 	}
+	char *segMap = new char[64];
+	ifstream file ("DRIVE/CHECKPOINT_REGION");
+	bool found = false;
+	int position = 160;
+	file.seekg(position);
+	file.read(segMap, 64);
+	int index = 0;
+	while(!found){
+		if(index == 64){
+			cout << "All segments are full, there is not enough memory left" << endl;
+		}
+		if(segMap[index] != 0){
+			found = true;
+			index--;
+		}
+		index++;
+	}
+	s->segNum = index;
+	file.close();
 } 
+
 void nextByte(segment *s){ // access next byte in segment
 }
 void nextBlock(segment *s){ // access next block in segment
